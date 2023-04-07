286f0e84561e70f049bcd9f6e1f38b14b976019c
Harsh Vishwakarma <42278251+CodeBlack03@users.noreply.github.com>
2023-04-07 21:20:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-'''Train CIFAR10 with PyTorch.'''
-from __future__ import print_function
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,121 +13,64 @@ import argparse
 from tqdm import tqdm
 import random
 import numpy as np
-from models.wideresnet import *
+from model.wideN import *
+from utils import *
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--seed', default=11111, type=int)
-parser.add_argument('--momentum', default=0.9, type=float)
-parser.add_argument('--weight_decay', default=5e-4, type=float)
-parser.add_argument('--epsilon', default=8.0/255, type=float)
-parser.add_argument('--m', default=8, type=int)
-parser.add_argument('--batch_size', default=128, type=int)
-parser.add_argument('--resume', '-r', default=None, type=int, help='resume from checkpoint')
-args = parser.parse_args()
+EPOCHS,BATCH_SIZE,ITERATION,EPSILON,STEP_SIZE,SEED,DEVICE,M,LR,MOMENT,WEIGHT_DECAY = init_args()
+random.seed(SEED)
+torch.cuda.manual_seed(SEED) #Set seed for cuda
+torch.manual_seed(SEED) #Set seed for cpu
+np.random.seed(SEED) #Set seed for NumPy
+torch.backends.cudnn.deterministic = True #Set deterministic behaviour of cuda
+torch.backends.cudnn.benchmark = True #allow PyTorch to optimize GPU performance dynamically based on the input size 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+trainLoader,testLoader = import_data(BATCH_SIZE)
+net34 = WideResNet_34_10()
+delta = torch.zeros(BATCH_SIZE, 3, 32, 32)
+delta = delta.to(DEVICE)
+net34 = net34.to(DEVICE)
 
-seed = args.seed
-random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-np.random.seed(seed)
-torch.backends.cudnn.deterministic=True
-torch.backends.cudnn.benchmark = False
+#Allow model to parallely use GPU's
+if(DEVICE=="cuda" and torch.cuda.device_count() > 1):
+    net34 = nn.DataParallel(net34, list(range(torch.cuda.device_count())))
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    # Normalization messes with l-inf bounds.
-])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, drop_last=True)
+optimizer = optim.SGD(net34.parameters(),lr=LR,momentum=MOMENT,weight_decay=WEIGHT_DECAY)
 
 
-
-print('==> Building model..')
-net = WideResNet_28_10()
-epsilon = args.epsilon
-m = args.m
-delta = torch.zeros(args.batch_size, 3, 32, 32)
-delta = delta.to(device)
-net = net.to(device)
+def train():
+    accuracy_per_epoch=[]
+    C_loss = nn.CrossEntropyLoss()
+    #Initialise Optimizer as Stochastic Gradient Descent
 
 
-if args.resume is not None:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.{}'.format(args.resume))
-    net.load_state_dict(checkpoint['net'])
-    start_epoch = checkpoint['epoch'] + 1
-    torch.set_rng_state(checkpoint['rng_state'])
+    for epoch in (range(EPOCHS//M)):
+        net34.train()
+        global delta
+        iterator = tqdm(trainLoader,ncols=0)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.1 if epoch<12 else (0.01 if (epoch >= 12 and epoch < 22) else 0.001)
+        for batch_idx,(x_train, y_train) in enumerate(iterator):
+            x_train = x_train.to(DEVICE)
+            y_train = y_train.to(DEVICE)
+            for _ in range(M):
+                optimizer.zero_grad()
 
-optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=args.momentum, weight_decay=args.weight_decay)
+                x_adv = (x_train+delta).detach()
+                x_adv.requires_grad_()
+                y_pred = net34(x_adv)
+                
+                loss = C_loss(y_pred,y_train)
+                loss.backward()
+                optimizer.step() #theta = theta - lr*G(theta)
+                grad = x_adv.grad.data
+                delta = delta.detach() + EPSILON*torch.sign(grad.detach())
+                delta = torch.clamp(delta,-EPSILON,EPSILON)
+                
 
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    iterator = tqdm(trainloader, ncols=0, leave=False)
-    global delta
-
-    for batch_idx, (inputs, targets) in enumerate(iterator):
-        inputs, targets = inputs.to(device), targets.to(device)
-        for i in range(m):
-            optimizer.zero_grad()
-            adv = (inputs+delta).detach()
-            adv.requires_grad_()
-            outputs = net(adv)
-            loss = F.cross_entropy(outputs, targets)
-            loss.backward()
-            optimizer.step()
-            grad = adv.grad.data
-            delta = delta.detach() + epsilon * torch.sign(grad.detach())
-            delta = torch.clamp(delta, -epsilon, epsilon)
-
-            train_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-            iterator.set_description(str(predicted.eq(targets).sum().item()/targets.size(0)))
-
-    acc = 100.*correct/total
-    print('Train acc:', acc)
-
-    
-    print('Saving..')
-    state = {
-        'net': net.state_dict(),
-        'acc': acc,
-        'epoch': epoch,
-        'rng_state': torch.get_rng_state()
-    }
-    if not os.path.isdir('checkpoint'):
-        os.mkdir('checkpoint')
-    torch.save(state, './checkpoint/ckpt.{}'.format(epoch))
-    best_acc = acc
+        net34.eval()
+        accuracy_per_epoch.append(float(accuracy(model, test_loader, device)))
+        print(f'Accuracy at epoch {epoch}: {accuracy_per_epoch[-1]}')
 
 
-def adjust_learning_rate(optimizer, epoch):
-    if epoch < 12:
-        lr = 0.1
-    elif epoch >= 12 and epoch < 22:
-        lr = 0.01
-    elif epoch >= 22: 
-        lr = 0.001
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-
-for epoch in range(start_epoch, 27):
-    adjust_learning_rate(optimizer, epoch)
-    train(epoch)
+if __name__ == '__main__':
+    train()
--- utils.py
+++ utils.py
@@ -1,124 +1,45 @@
-'''Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
-    - msr_init: net parameter initialization.
-    - progress_bar: progress bar mimic xlua.progress.
-'''
-import os
-import sys
-import time
-import math
 
-import torch.nn as nn
-import torch.nn.init as init
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
+def import_data(batch_size):
+    transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
 
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
+    ])
+    transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    ])
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, drop_last=True)
+    testloader = torch.utils.data.DataLoader(testset,batch_size=batch_size, shuffle=False,num_workers=2)
+    return trainloader,testloader
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
+def init_args():
+    EPOCH=27
+    BATCH_SIZE=128
+    ITERATION=100
+    EPSILON=8.0/255
+    STEP_SIZE=2./255
+    SEED=99
+    M=8
+    LR=0.1
+    MOMENT=0.9
+    WEIGHT_DECAY=5e-4
+    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return EPOCH,BATCH_SIZE,ITERATION,EPSILON,STEP_SIZE,SEED,DEVICE,M,LR,MOMENT,WEIGHT_DECAY
 
-TOTAL_BAR_LENGTH = 65.
-last_time = time.time()
-begin_time = last_time
-def progress_bar(current, total, msg=None):
-    global last_time, begin_time
-    if current == 0:
-        begin_time = time.time()  # Reset for new bar.
 
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
-    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
-
-    sys.stdout.write(' [')
-    for i in range(cur_len):
-        sys.stdout.write('=')
-    sys.stdout.write('>')
-    for i in range(rest_len):
-        sys.stdout.write('.')
-    sys.stdout.write(']')
-
-    cur_time = time.time()
-    step_time = cur_time - last_time
-    last_time = cur_time
-    tot_time = cur_time - begin_time
-
-    L = []
-    L.append('  Step: %s' % format_time(step_time))
-    L.append(' | Tot: %s' % format_time(tot_time))
-    if msg:
-        L.append(' | ' + msg)
-
-    msg = ''.join(L)
-    sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
-
-    # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
-
-    if current < total-1:
-        sys.stdout.write('\r')
-    else:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
-
-def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
-    hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
-    minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
-    secondsf = int(seconds)
-    seconds = seconds - secondsf
-    millis = int(seconds*1000)
-
-    f = ''
-    i = 1
-    if days > 0:
-        f += str(days) + 'D'
-        i += 1
-    if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
-        i += 1
-    if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
-        i += 1
-    if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
-        i += 1
-    if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
-        i += 1
-    if f == '':
-        f = '0ms'
-    return f
+def accuracy(net, loader, device):
+    acc = torchmetrics.Accuracy(task='multiclass', num_classes=10).to(device)
+    for xb, yb in loader:
+        xb, yb = xb.to(device), yb.to(device)
+        ypred = net(xb)
+        _ = acc(ypred, yb)
+    return acc.compute()
